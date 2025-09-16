ba3079f47aa12215b81bb0a58030826f1182c449
dev-tpghks23 <tpghksgit23@gmail.com>
2025-09-16 17:50:05 +0900

    day05

--- day04/Scripts/dml.sql
+++ day04/Scripts/dml.sql
@@ -267,13 +267,73 @@ WHERE USER_ID IN (
 );
 
 -- 6) 서울에 살고 있는 인원 수에 해당하는 번호에 게시글 조회
+
+SELECT * 
+FROM TBL_POST 
+WHERE ID = (
+	SELECT COUNT(ID)
+	FROM TBL_USER
+	WHERE USER_ADDRESS LIKE '%서울%'
+);
+
 -- 7) 주소가 '구'로 끝나는 유저가 작성한 게시글에 달린 모든 댓글들 조회
+SELECT *
+FROM TBL_REPLY
+WHERE POST_ID IN (
+	SELECT ID
+	FROM TBL_POST
+	WHERE USER_ID IN (
+		SELECT ID
+		FROM TBL_USER
+		WHERE USER_ADDRESS LIKE '%구'
+	)
+);
+
 -- 8) 댓글에 '한민'이가 포함된 게시글에 달린 모든 댓글 조회
+
+SELECT *
+FROM TBL_REPLY
+WHERE POST_ID IN (
+	SELECT ID
+	FROM TBL_POST
+	WHERE ID IN (
+		SELECT POST_ID
+		FROM TBL_REPLY
+		WHERE REPLY_CONTENT LIKE '%한민%'
+	)
+);
+
 -- 9) 평균 댓글 개수보다 많이 달린 게시글을 작성한 유저
+SELECT * 
+FROM TBL_USER
+WHERE ID IN (
+	SELECT USER_ID
+	FROM TBL_POST
+	WHERE ID IN (
+		SELECT POST_ID
+		FROM TBL_REPLY
+		GROUP BY POST_ID
+		HAVING COUNT(POST_ID) > (
+			SELECT AVG(COUNT(POST_ID))
+			FROM TBL_REPLY
+			GROUP BY POST_ID
+		)
+	)
+);
+
 -- 10) 가장 댓글을 적게 작성한 유저가 작성한 게시글
-
-
-
+SELECT * 
+FROM TBL_POST 
+WHERE USER_ID IN (
+	SELECT USER_ID
+	FROM (
+		SELECT USER_ID
+		FROM TBL_REPLY
+		GROUP BY USER_ID
+		ORDER BY COUNT(USER_ID)
+	)
+	WHERE ROWNUM <= 4
+);
 
 
 
